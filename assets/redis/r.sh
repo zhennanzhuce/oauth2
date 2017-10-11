6c54eca9b2d8dcf0ba8531417d4eeac5a27d9006
speedt <13837186852@qq.com>
2017-10-11 11:25:01 +0800

    haha

---
+++ assets/redis/r.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "authorize.lua"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/authorize.lua)"
+
+echo ""
+echo "getSession.lua"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/getSession.lua)"
+
+echo ""
+echo "pw.lua"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/pw.lua)"
+
+echo ""
+echo "token.lua"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/token.lua)"
