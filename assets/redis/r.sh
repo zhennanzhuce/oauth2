a57027863e1e073dc94434d7d1ed629f24631daa
speedt <13837186852@qq.com>
2017-10-11 11:26:56 +0800

    haha

--- assets/redis/r.sh
+++ assets/redis/r.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "authorize.lua"
-/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/authorize.lua)"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 -p 12379 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/authorize.lua)"
 
 echo ""
 echo "getSession.lua"
-/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/getSession.lua)"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 -p 12379 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/getSession.lua)"
 
 echo ""
 echo "pw.lua"
-/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/pw.lua)"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 -p 12379 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/pw.lua)"
 
 echo ""
 echo "token.lua"
-/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/token.lua)"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 -p 12379 script load "$(cat /root/my/git/zhennanzhuce/oauth2/assets/redis/token.lua)"
